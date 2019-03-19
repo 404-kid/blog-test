97e5f37f30db50f956d08f861243bd297bb05bab
Job <jobsmith9@gmail.com>
2019-03-18 19:06:55 -0600

    Updated admin toools

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { browserHistory } from 'react-router';
+
+// import Routes from './routes';
+
+// ReactDOM.render(
+//  <Routes history={browserHistory} />,
+//  document.getElementById('root')
+// );
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import { browserHistory } from 'react-router';
+
+// import Routes from './routes';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
